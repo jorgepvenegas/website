80670fe9b21f0a0e9c9fc1a29f4b41a54b3b0d27
Jorge Venegas <jorgepvenegas@gmail.com>
2019-09-09 21:51:17 -0700

    GA tracking plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,12 @@ module.exports = {
         icon: `src/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-71489910-1",
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
