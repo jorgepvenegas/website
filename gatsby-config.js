de5e9eba1128d9e1f6b478175eb76de98f6f898c
Jorge Venegas <jorgepvenegas@protonmail.com>
2021-08-18 23:45:56 -0700

    ooops

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,23 +15,9 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/icon.png',
-      },
-    },
     'gatsby-plugin-mdx',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: './src/images/',
-      },
-      __key: 'images',
-    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
